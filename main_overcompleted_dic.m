e141670dcd2b9b4adf8090d02254054f1a0552f4
miguelrivascosta <rivascostam@gmail.com>
2025-11-13 19:35:38 +0100

    Estimator of Partially-Connected

--- main_overcompleted_dic.m
+++ main_overcompleted_dic.m
@@ -1,8 +1,8 @@
 clear, close all
 
 rng default
-n = 8;
-nrf = 4;
+n = 4;
+nrf = 2;
 Q  = n/nrf;
 bq = 2*Q-1;
 angles = 2*pi/bq*((0:bq-1)-(bq-1)/bq);
@@ -22,6 +22,23 @@ R = toeplitz(rk,conj(rk));
 r = [R(1,end:-1:1,:).';R(2:end,1)];
 
 
+Sigma = zeros(nrf*nrf,2*nrf-1);
+Sigma(:,nrf) = reshape(eye(nrf),[nrf*nrf,1]);
+for i = 1 : nrf-1
+    Qi = [zeros(nrf-i,i), eye(nrf-i);zeros(i,nrf-i), zeros(i,i)];
+    Sigma(:,nrf-i) = reshape(Qi,[nrf*nrf,1]);
+    Sigma(:,nrf+i) = reshape(Qi.',[nrf*nrf,1]); 
+end
+pinv_Sigma = (Sigma.'*Sigma)\Sigma.';
+Sm_dic = pagemtimes(pagemtimes(pagectranspose(codebook),R),codebook);
+sm_tilde = zeros(2*nrf-1,M);
+sm_vec = zeros(nrf*nrf,M);
+for m = 1 : M
+    Sm = Sm_dic(:,:,m);
+    sm_vec(:,m) = Sm(:);
+    sm_tilde(:,m) = pinv_Sigma*Sm(:);
+end
+s_vec = sm_vec(:);
 
 
 r_real = zeros(2*n-1,1);
@@ -41,72 +58,73 @@ P = [];
 %     P = [P;Pq];
 % end
 
+
+
 for q = 0:2*nrf-2
     Puv = [zeros(2*Q-1,q*Q),eye(2*Q-1),zeros(2*Q-1,2*n-(q+2)*Q)];
     P = [P;Puv];
 end
 
-
-
-
-Sigma = zeros(nrf*nrf,2*nrf-1);
-Sigma(:,nrf) = reshape(eye(nrf),[nrf*nrf,1]);
-for i = 1 : nrf-1
-    Qi = [zeros(nrf-i,i), eye(nrf-i);zeros(i,nrf-i), zeros(i,i)];
-    Sigma(:,nrf-i) = reshape(Qi,[nrf*nrf,1]);
-    Sigma(:,nrf+i) = reshape(Qi.',[nrf*nrf,1]); 
+q_vec = (-(Q-1):Q-1).';
+a = 1-abs(q_vec)/Q;
+Omega_ext = [];
+Omega = a.'.*exp(-1j*angles.'.*q_vec.');
+I_bnrf = eye(2*nrf-1);
+for m = 0 : M-1
+    phi_m = 2*pi/(2*Q-1)*(m-(2*Q-2)/(2*Q-1));
+    fm = exp(-1j*phi_m*q_vec);
+    Omega_m = kron(I_bnrf,(a.*fm).');
+    em = zeros(2*Q-1,1);
+    em(m+1) = 1;
+    
+    sel = kron(eye(2*nrf-1),em.'*Omega);
+    aux = reshape(Sigma*sel*P*r,[nrf,nrf]);
+    norm(Sm_dic(:,:,m+1)-aux)
+    
+    Omega_ext = [Omega_ext;sel];
 end
 
+norm(s_vec-kron(eye(M),Sigma)*Omega_ext*P*r)
 
 
 
-pinv_Sigma = (Sigma.'*Sigma)\Sigma.';
 
 
-Sm_dic = pagemtimes(pagemtimes(pagectranspose(codebook),R),codebook);
-sm_tilde = zeros(2*nrf-1,M);
-sm_vec = zeros(nrf*nrf,M);
-for m = 1 : M
-    Sm = Sm_dic(:,:,m);
-    sm_vec(:,m) = Sm(:);
-    sm_tilde(:,m) = pinv_Sigma*Sm(:);
-end
-s_vec = sm_vec(:);
 
-toeplitz_mat = zeros(2*n-1,2*n-1);
-
-q_vec = -(Q-1):(Q-1);
-aq = 1 - abs(q_vec)/Q;
-
-u = 3;
-Psi = [];
-for u = 1 : M
-    phi_u = angles(u);
-    
-    
-    Psi_u = zeros(2*nrf-1,2*n-1);
-    Psi_u(1,1) = aq(Q);
-    Psi_u(1,2:2:2*Q-1) = 2*aq(Q+1:2*Q-1).*cos(phi_u*q_vec(Q+1:2*Q-1));
-    Psi_u(1,3:2:2*Q-1) = 2*aq(Q+1:2*Q-1).*sin(phi_u*q_vec(Q+1:2*Q-1));
-    
-    idx_real_ini = 1:2:2*(2*Q-1);
-    idx_imag_ini = 2:2:2*(2*Q-1);
-    for i = 1:nrf-1
-        idx_real = (idx_real_ini + (i-1)*2*Q)+1;
-        idx_imag = (idx_imag_ini + (i-1)*2*Q)+1;
-    
-        Psi_u(2*i,idx_real) = aq.*exp(-1j*phi_u*q_vec);
-        Psi_u(2*i,idx_imag) = 1j*aq.*exp(-1j*phi_u*q_vec);
-        Psi_u(2*i+1,idx_real) = aq.*exp(1j*phi_u*q_vec);
-        Psi_u(2*i+1,idx_imag) = -1j*aq.*exp(1j*phi_u*q_vec);
-    end
-    Psi = [Psi;Psi_u];
-end
-Sigma_ext = kron(eye(M),Sigma);
-Gamma = Sigma_ext*Psi;
-
-pinv_Gamma = (Gamma'*Gamma)\Gamma';
-norm(r_real-pinv_Gamma*s_vec)
+% toeplitz_mat = zeros(2*n-1,2*n-1);
+% 
+% q_vec = -(Q-1):(Q-1);
+% aq = 1 - abs(q_vec)/Q;
+% 
+% u = 3;
+% Psi = [];
+% for u = 1 : M
+%     phi_u = angles(u);
+% 
+% 
+%     Psi_u = zeros(2*nrf-1,2*n-1);
+%     Psi_u(1,1) = aq(Q);
+%     Psi_u(1,2:2:2*Q-1) = 2*aq(Q+1:2*Q-1).*cos(phi_u*q_vec(Q+1:2*Q-1));
+%     Psi_u(1,3:2:2*Q-1) = 2*aq(Q+1:2*Q-1).*sin(phi_u*q_vec(Q+1:2*Q-1));
+% 
+%     idx_real_ini = 1:2:2*(2*Q-1);
+%     idx_imag_ini = 2:2:2*(2*Q-1);
+%     for i = 1:nrf-1
+%         idx_real = (idx_real_ini + (i-1)*2*Q)+1;
+%         idx_imag = (idx_imag_ini + (i-1)*2*Q)+1;
+% 
+%         Psi_u(2*i,idx_real) = aq.*exp(-1j*phi_u*q_vec);
+%         Psi_u(2*i,idx_imag) = 1j*aq.*exp(-1j*phi_u*q_vec);
+%         Psi_u(2*i+1,idx_real) = aq.*exp(1j*phi_u*q_vec);
+%         Psi_u(2*i+1,idx_imag) = -1j*aq.*exp(1j*phi_u*q_vec);
+%     end
+%     Psi = [Psi;Psi_u];
+% end
+% Sigma_ext = kron(eye(M),Sigma);
+% Gamma = Sigma_ext*Psi;
+% 
+% pinv_Gamma = (Gamma'*Gamma)\Gamma';
+% norm(r_real-pinv_Gamma*s_vec)
 
 
 
